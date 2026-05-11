149bd7991af41f04ce486cc52208de0a604d3a43
Ali Hassan <21276179+Ali0600@users.noreply.github.com>
2026-05-11 12:48:27 +0200

    layout test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Capture',
+          title: 'Capture2',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
         }}
       />
